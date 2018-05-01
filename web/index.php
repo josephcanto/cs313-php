cdaf1c34a04f735d7be009a3029ea87bd91e3af0
Joseph Canto <can16004@byui.edu>
2018-05-01 16:33:29 -0600

    PHP test

--- web/index.php
+++ web/index.php
@@ -1,5 +1,12 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <?php echo "This is a PHP page."?>
+</body>
+</html>
