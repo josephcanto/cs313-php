ae9e1818c20f8b4b5522c3cac2013e659f4118cf
Joseph Canto <can16004@byui.edu>
2018-05-03 16:55:34 -0600

    adding link to group assignment from week 2

--- web/index.php
+++ web/index.php
@@ -9,6 +9,7 @@
 <body>
   <?php
     echo "<p>This is a PHP page.</p>";
+    echo "<a href='/week02/home.php' title='see week 2 group assignment'>Week 2 Group Assignment</a>";
     $username = filter_input(INPUT_GET, 'username');
     if(isset($username)) {
       echo "<p>Welcome $username!</p>";
