a48bf33574854e66fd05922265f43a09df69083f
Joseph Canto <can16004@byui.edu>
2018-05-05 17:25:20 -0600

    closed anchor tags in navigation links

--- web/modules/nav.php
+++ web/modules/nav.php
@@ -1,6 +1,6 @@
 <nav>
     <ul class='main-nav'>
-        <li><a href='index.php' title='Go to the home page'>Home</li>
-        <li><a href='assignments.php' title='Go to the assignments page'>Assignments</li>
+        <li><a href='index.php' title='Go to the home page'>Home</a></li>
+        <li><a href='assignments.php' title='Go to the assignments page'>Assignments</a></li>
     </ul>
 </nav>
