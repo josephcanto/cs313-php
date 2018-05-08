b2debb698e37db865ee9e5dd1f7076c0e59f11df
Joseph Canto <can16004@byui.edu>
2018-05-08 15:58:01 -0600

    added some br tags

--- web/week03/form.php
+++ web/week03/form.php
@@ -9,16 +9,16 @@
 <body>
     <form action='script.php' method='post'>
         <label for='name'>Name:</label>
-        <input type='text' id='name' name='name'>
+        <input type='text' id='name' name='name'><br>
         <label for='email'>Email:</label>
-        <input type='email' id='email' name='email'>
-        <label>Major:</label>
-        <input type='radio' name='major' value='Computer Science'>
-        <input type='radio' name='major' value='Web Design and Development'>
-        <input type='radio' name='major' value='Computer information Technology'>
-        <input type='radio' name='major' value='Computer Engineering'>
+        <input type='email' id='email' name='email'><br>
+        <label>Major:</label><br>
+        <input type='radio' name='major' value='Computer Science'><br>
+        <input type='radio' name='major' value='Web Design and Development'><br>
+        <input type='radio' name='major' value='Computer information Technology'><br>
+        <input type='radio' name='major' value='Computer Engineering'><br>
         <label for='comments'>Comments:</label>
-        <textarea rows='4' cols='50'></textarea>
+        <textarea rows='4' cols='50'></textarea><br>
         <input type='submit' value='Submit'>
     </form>
 </body>
