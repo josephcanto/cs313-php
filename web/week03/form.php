51710a118ac76abb53ee0e0a710bdd9597cc4378
Joseph Canto <can16004@byui.edu>
2018-05-08 15:55:43 -0600

    created html form

---
+++ web/week03/form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action='script.php' method='post'>
+        <label for='name'>Name:</label>
+        <input type='text' id='name' name='name'>
+        <label for='email'>Email:</label>
+        <input type='email' id='email' name='email'>
+        <label>Major:</label>
+        <input type='radio' name='major' value='Computer Science'>
+        <input type='radio' name='major' value='Web Design and Development'>
+        <input type='radio' name='major' value='Computer information Technology'>
+        <input type='radio' name='major' value='Computer Engineering'>
+        <label for='comments'>Comments:</label>
+        <textarea rows='4' cols='50'></textarea>
+        <input type='submit' value='Submit'>
+    </form>
+</body>
+</html>
