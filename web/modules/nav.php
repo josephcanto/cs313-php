c9cb55ce250dee31ace723a39c510256ca32d59d
Joseph Canto <can16004@byui.edu>
2018-05-08 11:54:41 -0600

    added an active css class to indicate which link/page is active

--- web/modules/nav.php
+++ web/modules/nav.php
@@ -1,6 +1,6 @@
 <nav>
     <ul class='main-nav'>
-        <li><a href='index.php' title='Go to the home page'>Home</a></li>
-        <li><a href='assignments.php' title='Go to the assignments page'>Assignments</a></li>
+        <li><a href='index.php' title='Go to the home page' class='<?php if($_SERVER["PHP_SELF"] == 'index.php') echo 'active'; ?>'>Home</a></li>
+        <li><a href='assignments.php' title='Go to the assignments page' class='<?php if($_SERVER["PHP_SELF"] == 'assignments.php') echo 'active'; ?>'>Assignments</a></li>
     </ul>
 </nav>
