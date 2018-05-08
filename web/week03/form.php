4f2a1fa62928638b6a50ad7550fd93da8b19cdab
Joseph Canto <can16004@byui.edu>
2018-05-08 16:46:42 -0600

    changed title attributes

--- web/week03/form.php
+++ web/week03/form.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>PHP Form</title>
 </head>
 <body>
     <form action='script.php' method='post'>
--- web/week03/script.php
+++ web/week03/script.php
@@ -23,7 +23,7 @@ function test_input($data) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Display PHP Form Input</title>
 </head>
 <body>
     <?php
