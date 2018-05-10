e169cb371cb50d368df4faa453bc287b864a5569
Joseph Canto <can16004@byui.edu>
2018-05-10 16:34:27 -0600

    added folders for group assignments from week 3 and for the shopping cart assignment from week 3

--- web/assignments.php
+++ web/assignments.php
@@ -14,8 +14,9 @@
         <ul class='assignment-list'>
             <li><a href='week01/hello.html' title='See my Hello World assignment from Week 1'>Week 1 - Hello World Assignment</a></li>
             <li><a href='week02/home.php' title="See my group's assignment from Week 2">Week 2 - Group Assignment</a></li>
-            <li><a href='week03/form.php' title="See my group's assignment from Week 3">Week 3 - PHP Form Group Assignment</a></li>
-            <li><a href='week03/page-counter.php' title="See my group's second assignment from Week 3">Week 3 - Page Visit Counter Group Assignment</a></li>
+            <li><a href='week03/group-assignments/form.php' title="See my group's assignment from Week 3">Week 3 - PHP Form Group Assignment</a></li>
+            <li><a href='week03/group-assignments/page-counter.php' title="See my group's second assignment from Week 3">Week 3 - Page Visit Counter Group Assignment</a></li>
+            <li><a href='week03/shopping-cart/browse.php' title="See my Shopping Cart assignment from Week 3">Week 3 - Shopping Cart Assignment</a></li>
         </ul>
     </main>
     <!-- <?php require 'modules/footer.php'; ?> -->
