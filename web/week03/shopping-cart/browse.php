d73d140c5bdf6936b8a7b3010582b11af05b3b52
Joseph Canto <can16004@byui.edu>
2018-05-12 19:58:14 -0600

    changed css styles

--- web/week03/shopping-cart/browse.php
+++ web/week03/shopping-cart/browse.php
@@ -12,84 +12,84 @@
     <div id='container'>
         <div>
             <figure>
-                <img src='images/t-shirt-1.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-1.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $10.50</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
         </div>
         <div>
             <figure>
-                <img src='images/t-shirt-2.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-2.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $12.25</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
         </div>
         <div>
             <figure>
-                <img src='images/t-shirt-3.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-3.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $11.35</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
         </div>
         <div>
             <figure>
-                <img src='images/t-shirt-4.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-4.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $15.40</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
         </div>
         <div>
             <figure>
-                <img src='images/t-shirt-5.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-5.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $8.92</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
         </div>
         <div>
             <figure>
-                <img src='images/t-shirt-6.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-6.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $13.11</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
         </div>
         <div>
             <figure>
-                <img src='images/t-shirt-7.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-7.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $9.73</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
         </div>
         <div>
             <figure>
-                <img src='images/t-shirt-8.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-8.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $10.38</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
         </div>
         <div>
             <figure>
-                <img src='images/t-shirt-9.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-9.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $12.22</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
         </div>
         <div>
             <figure>
-                <img src='images/t-shirt-10.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-10.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $14.71</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
         </div>
         <div>
             <figure>
-                <img src='images/t-shirt-11.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-11.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $15.87</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
         </div>
         <div>
             <figure>
-                <img src='images/t-shirt-12.jpg' alt='Rocket League T-Shirt' width='300' height='300'>
+                <img src='images/t-shirt-12.jpg' alt='Rocket League T-Shirt'>
                 <figcaption>Men's T-Shirt, Size: M, Price: $14.88</figcaption>
             </figure>
             <button type='button' onclick='addToCart()' title='Add item to your cart'>Add To Cart</button>
