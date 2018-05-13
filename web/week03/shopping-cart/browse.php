ec0db2bca2d46fbfefa9ccf55e32ce1a4a57cb19
Joseph Canto <can16004@byui.edu>
2018-05-12 23:42:36 -0600

    removed jquery import

--- web/week03/shopping-cart/browse.php
+++ web/week03/shopping-cart/browse.php
@@ -153,11 +153,11 @@
             </form>
         </div>
     </div>
-    <script
+    <!-- <script
         src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
         crossorigin="anonymous">
-    </script>
+    </script> -->
     <!-- <script src="jquery/script.js"></script> -->
 </body>
 </html>
