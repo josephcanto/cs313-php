8f20e9bb2100fa2017f9fc8ec9e9eb80439b62c2
Joseph Canto <can16004@byui.edu>
2018-05-12 19:38:38 -0600

    adding starter files for shopping cart assignment

--- web/week03/shopping-cart/browse.php
+++ web/week03/shopping-cart/browse.php
@@ -7,6 +7,80 @@
     <title>Browse</title>
 </head>
 <body>
-    <h1>Coming soon</h1>
+    <h1>Browse our selection of Rocket League T-shirts</h1>
+    <div id='container'>
+        <div>
+            <figure>
+                <img src='images/t-shirt-1' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$10.50</figcaption>
+            </figure>
+        </div>
+        <div>
+            <figure>
+                <img src='images/t-shirt-2' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$12.25</figcaption>
+            </figure>
+        </div>
+        <div>
+            <figure>
+                <img src='images/t-shirt-3' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$11.35</figcaption>
+            </figure>
+        </div>
+        <div>
+            <figure>
+                <img src='images/t-shirt-4' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$15.40</figcaption>
+            </figure>
+        </div>
+        <div>
+            <figure>
+                <img src='images/t-shirt-5' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$8.92</figcaption>
+            </figure>
+        </div>
+        <div>
+            <figure>
+                <img src='images/t-shirt-6' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$13.11</figcaption>
+            </figure>
+        </div>
+        <div>
+            <figure>
+                <img src='images/t-shirt-7' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$9.73</figcaption>
+            </figure>
+        </div>
+        <div>
+            <figure>
+                <img src='images/t-shirt-8' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$10.38</figcaption>
+            </figure>
+        </div>
+        <div>
+            <figure>
+                <img src='images/t-shirt-9' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$12.22</figcaption>
+            </figure>
+        </div>
+        <div>
+            <figure>
+                <img src='images/t-shirt-10' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$14.71</figcaption>
+            </figure>
+        </div>
+        <div>
+            <figure>
+                <img src='images/t-shirt-11' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$15.87</figcaption>
+            </figure>
+        </div>
+        <div>
+            <figure>
+                <img src='images/t-shirt-12' alt='Rocket League T-Shirt' width='300' height='300'>
+                <figcaption>$14.88</figcaption>
+            </figure>
+        </div>
+    </div>
 </body>
 </html>
