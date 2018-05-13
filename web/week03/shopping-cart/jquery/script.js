d190355cdbf501bbac6df8345eca1c2d661f7781
Joseph Canto <can16004@byui.edu>
2018-05-12 22:20:46 -0600

    testing

--- web/week03/shopping-cart/jquery/script.js
+++ web/week03/shopping-cart/jquery/script.js
@@ -1,6 +1,6 @@
 $('button').click(function(e) {
-    alert("button with id " + this.id + " was clicked");
-    // e.preventDefault();
+    // alert("button with id " + this.id + " was clicked");
+    e.preventDefault();
     $.ajax({
         type: "POST",
         url: "../add.php",
