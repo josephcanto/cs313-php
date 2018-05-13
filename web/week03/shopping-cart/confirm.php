455dd6d366338dc67a730fe001ef807285a4085c
Joseph Canto <can16004@byui.edu>
2018-05-12 20:45:42 -0600

    linked css to other pages

--- web/week03/shopping-cart/confirm.php
+++ web/week03/shopping-cart/confirm.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Order Received Confirmation</title>
+    <link rel='stylesheet' type='text/css' href='css/styles.css'>
+    <title>Thank You! | Order Received Confirmation</title>
 </head>
 <body>
     <h1>Your order has been received and is being processed. You should expect to receive it within 3-5 business days. Thank you, and have a nice day!</h1>
