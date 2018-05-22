15288466a39f9fc397776d7eb689cf991e0d98b5
Joseph Canto <can16004@byui.edu>
2018-05-22 14:43:42 -0600

    moved PHP code into the same block as the foreach loop

--- web/php_project/connection.php
+++ web/php_project/connection.php
@@ -1,22 +1,3 @@
-<?php
-    $dbUrl = getenv('DATABASE_URL');
-
-    $dbopts = parse_url($dbUrl);
-
-    $dbHost = $dbopts["host"];
-    $dbPort = $dbopts["port"];
-    $dbUser = $dbopts["user"];
-    $dbPassword = $dbopts["pass"];
-    $dbName = ltrim($dbopts["path"],'/');
-
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-    // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $stmt = $db->prepare('SELECT * FROM users WHERE id=:id AND lastname=:lastname');
-    $stmt->execute(array(':lastname' => $lastname, ':id' => $id));
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
@@ -28,8 +9,26 @@
 <body>
     <h1>Awesome web app coming soon!</h1>
     <?php
+        $dbUrl = getenv('DATABASE_URL');
+
+        $dbopts = parse_url($dbUrl);
+    
+        $dbHost = $dbopts["host"];
+        $dbPort = $dbopts["port"];
+        $dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];
+        $dbName = ltrim($dbopts["path"],'/');
+    
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+        // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+        $stmt = $db->prepare('SELECT * FROM users WHERE id=:id AND lastname=:lastname');
+        $stmt->execute(array(':lastname' => $lastname, ':id' => $id));
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         foreach($rows as $row) {
-            echo 'User ID: ' . $row['id'] . ' Last Name: ' . $row['lastname'];
+            echo '<p>User ID: ' . $row['id'] . '<br>Last Name: ' . $row['lastname'] . '</p><br>';
         }
     ?>
 </body>
