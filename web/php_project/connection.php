89ba1a7de71f57403dc7c44df3f0021fb7e5cdc2
Joseph Canto <can16004@byui.edu>
2018-05-22 13:04:39 -0600

    created PHP file to test connecting to the database

---
+++ web/php_project/connection.php
@@ -0,0 +1,27 @@
+<?php
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbopts = parse_url($dbUrl);
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Connection Test</title>
+</head>
+<body>
+    <h1>Awesome web app coming soon!</h1>
+</body>
+</html>
