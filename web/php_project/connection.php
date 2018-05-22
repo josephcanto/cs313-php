10a66a5caf54eef6daab093978b87cc9184a211e
Joseph Canto <can16004@byui.edu>
2018-05-22 14:41:26 -0600

    added a PHP foreach loop to display the data retrieved from the database

--- web/php_project/connection.php
+++ web/php_project/connection.php
@@ -11,7 +11,11 @@
 
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $db->prepare('SELECT * FROM users WHERE id=:id AND lastname=:lastname');
+    $stmt->execute(array(':lastname' => $lastname, ':id' => $id));
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
@@ -23,5 +27,10 @@
 </head>
 <body>
     <h1>Awesome web app coming soon!</h1>
+    <?php
+        foreach($rows as $row) {
+            echo 'User ID: ' . $row['id'] . ' Last Name: ' . $row['lastname'];
+        }
+    ?>
 </body>
 </html>
