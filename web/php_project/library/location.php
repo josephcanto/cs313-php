cce4f1d76f3b923f3208caa7deae1b42838b0734
Joseph Canto <can16004@byui.edu>
2018-05-27 02:19:24 -0600

    apparently I had typed in $giftid first, and then $giftId every time after that! oops.. no wonder it wasn't working!

--- web/php_project/library/location.php
+++ web/php_project/library/location.php
@@ -4,23 +4,19 @@
 
     session_start();
 
-    $giftid = htmlspecialchars($_GET['giftid']);
-    var_dump($giftId);
+    $giftId = htmlspecialchars($_GET['giftid']);
     $giftName = getNameByGiftId($giftId);
-    var_dump($giftName);
     $_SESSION['giftName'] = $giftName['name'];
     $locationInfo = getLocationsByGiftId($giftId);
-    var_dump($locationInfo);
     if(!empty($locationInfo)) {
         $_SESSION['locationInfo'] = $locationInfo;
         $locationsList = buildLocationsList($locationInfo);
-        var_dump($locationsList);
     }
-    // if(!empty($locationsList)) {
-    //     $_SESSION['locationsList'] = $locationsList;
-    //     header('Location: ../view-location.php');
-    // } else {
-    //     $_SESSION['errorMessage'] = "<p class='notice'>Oops, something went wrong on our end.</p>";
-    //     header('Location: ../dashboard.php');
-    // }
+    if(!empty($locationsList)) {
+        $_SESSION['locationsList'] = $locationsList;
+        header('Location: ../view-location.php');
+    } else {
+        $_SESSION['errorMessage'] = "<p class='notice'>Oops, something went wrong on our end.</p>";
+        header('Location: ../dashboard.php');
+    }
 ?>
