731df1b990d3b72d773b71a7b4d34529dff30134
Joseph Canto <can16004@byui.edu>
2018-05-29 16:47:06 -0600

    testing

--- web/week06/insert_scriptures.php
+++ web/week06/insert_scriptures.php
@@ -1,19 +1,22 @@
 <?php
     session_start();
 
-    $dbUrl = getenv('DATABASE_URL');
+    function dbConnect() {
+        $dbUrl = getenv('DATABASE_URL');
 
-    $dbopts = parse_url($dbUrl);
-
-    $dbHost = $dbopts["host"];
-    $dbPort = $dbopts["port"];
-    $dbUser = $dbopts["user"];
-    $dbPassword = $dbopts["pass"];
-    $dbName = ltrim($dbopts["path"],'/');
-
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        $dbopts = parse_url($dbUrl);
+    
+        $dbHost = $dbopts["host"];
+        $dbPort = $dbopts["port"];
+        $dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];
+        $dbName = ltrim($dbopts["path"],'/');
+    
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);    
+    }
 
     function insertScripture($book, $chapter, $verse, $content) {
+        $db = dbConnect();
         $stmt = $db->prepare("INSERT INTO scriptures (book, chapter, verse, content)
         VALUES (:book, :chapter, :verse, :content)");
         $stmt->bindValue(':book', $book, PDO::PARAM_STR);
@@ -26,8 +29,8 @@
     }
 
     function insertScriptureTopic($topics) {
+        $db = dbConnect();
         $scripture_id = $pdo->lastInsertId('scripture_id_seq');
-
         $stmt = $db->prepare("INSERT INTO scripture_topics (scripture_id, topic_id)
         VALUES (:scripture_id, :topic_id");
         $stmt->bindValue(':scripture_id', $scripture_id, PDO::PARAM_INT);
@@ -39,6 +42,7 @@
 
     // function insertTopic($topics) {
     //     foreach($topics as $topic) {
+    //         $db = dbConnect();
     //         $stmt = $db->prepare("INSERT INTO topics (name)
     //         VALUES (:topic)");
     //         $stmt->bindValue(':topic', $topic['name'], PDO::PARAM_STR);
