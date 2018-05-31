605ef7210a6ccccfe023d1832578f3e020220468
Joseph Canto <can16004@byui.edu>
2018-05-31 17:35:36 -0600

    more testing

--- web/week06/course_notes_activity/insertNote.php
+++ web/week06/course_notes_activity/insertNote.php
@@ -11,15 +11,15 @@
     $content = htmlspecialchars($_POST["content"]);
     $date = htmlspecialchars($_POST["date"]);
 
-    // echo "Course: $courseId\n";
-    // echo "date: $date\n";
-    // echo "content: $content";
+    echo "Course: $courseId\n";
+    echo "date: $date\n";
+    echo "content: $content";
 
-    $query = "INSERT INTO note (course_id, content, date) VALUES (:courseId, :content, :date)";
-    $db->prepare($query);
-    $stmt->bindValue(':courseId', $courseId, PDO::PARAM_INT);
-    $stmt->bindValue(':content', $content, PDO::PARAM_STR);
-    $stmt->bindValue(':date', $date, PDO::PARAM_STR);
-    $stmt->execute();
-    header('Location: view-course.php?course_id=$courseId');
+    // $query = "INSERT INTO note (course_id, content, date) VALUES (:courseId, :content, :date)";
+    // $db->prepare($query);
+    // $stmt->bindValue(':courseId', $courseId, PDO::PARAM_INT);
+    // $stmt->bindValue(':content', $content, PDO::PARAM_STR);
+    // $stmt->bindValue(':date', $date, PDO::PARAM_STR);
+    // $stmt->execute();
+    // header('Location: view-course.php?course_id=$courseId');
 ?>
