e333b2e6c3a804960f9851c4f466d460f39ccf55
Joseph Canto <can16004@byui.edu>
2018-05-31 16:45:20 -0600

    testing php code

--- web/week06/course_notes_activity/insertNote.php
+++ web/week06/course_notes_activity/insertNote.php
@@ -1,9 +1,30 @@
 <?php
+    require 'dbConnect.php';
+
+    $db = get_db();
+
+    if(!isset($db)) {
+        die("DB Connection was not set");
+    }
+
     $courseId = htmlspecialchars($_POST["course_id"]);
     $date = htmlspecialchars($_POST["date"]);
     $content = htmlspecialchars($_POST["content"]);
 
-    echo "Course: $courseId\n";
-    echo "date: $date\n";
-    echo "content: $content";
+    // echo "Course: $courseId\n";
+    // echo "date: $date\n";
+    // echo "content: $content";
+
+    $query = "INSERT INTO note (course_id, content, date) VALUES (:courseId, :content, :date)";
+    $db->prepare($query);
+    $stmt->bindValue(':courseId', $courseId, PDO::PARAM_INT);
+    $stmt->bindValue(':content', $content, PDO::PARAM_STR);
+    $stmt->bindValue(':date', $date, PDO::PARAM_STR);
+    $stmt->execute();
+    $rowsChanged = $stmt->rowCount();
+    if($rowsChanged != 0) {
+        header('Location: view-course.php');
+    } else {
+        echo "<p>Note insert failed</p>";
+    }
 ?>
