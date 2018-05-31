ab9c9b7b89cc9b9bd7ce927bd4472d1737dc10fa
Joseph Canto <can16004@byui.edu>
2018-05-31 17:43:57 -0600

    fixed SQL insert query (hopefully!)

--- web/week06/course_notes_activity/insertNote.php
+++ web/week06/course_notes_activity/insertNote.php
@@ -16,7 +16,7 @@
     // echo "date: $date\n";
 
     $query = "INSERT INTO note (course_id, content, date) VALUES (:courseId, :content, :date)";
-    $db->prepare($query);
+    $stmt = $db->prepare($query);
     $stmt->bindValue(':courseId', $courseId, PDO::PARAM_INT);
     $stmt->bindValue(':content', $content, PDO::PARAM_STR);
     $stmt->bindValue(':date', $date, PDO::PARAM_STR);
