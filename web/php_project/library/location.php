6323e52c277e3fbc4904d4a7ab840a1ccb7af20d
Joseph Canto <can16004@byui.edu>
2018-06-03 00:07:49 -0600

    added code to store the gift id as a session variable

--- web/php_project/library/location.php
+++ web/php_project/library/location.php
@@ -5,15 +5,12 @@
     session_start();
 
     $giftId = htmlspecialchars($_GET['giftid']);
+    $_SESSION['giftId'] = $giftId;
     $giftName = getNameByGiftId($giftId);
     $_SESSION['giftName'] = $giftName['name'];
     $locationInfo = getLocationsByGiftId($giftId);
-    if(!empty($locationInfo)) {
-        $_SESSION['locationInfo'] = $locationInfo;
-        $locationsList = buildLocationsList($locationInfo);
-        if(!empty($locationsList)) {
-            $_SESSION['locationsList'] = $locationsList;
-        }
-    }
+    $_SESSION['locationInfo'] = $locationInfo;
+    $locationsList = buildLocationsList($locationInfo);
+    $_SESSION['locationsList'] = $locationsList;
     header('Location: ../view-location.php');
 ?>
