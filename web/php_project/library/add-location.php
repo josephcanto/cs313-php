de8bce5885cbe7649b75d31bca333f93e720cea2
Joseph Canto <can16004@byui.edu>
2018-06-03 00:52:12 -0600

    testing location form variables

--- web/php_project/library/add-location.php
+++ web/php_project/library/add-location.php
@@ -6,25 +6,27 @@
 
     $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
     $address = filter_input(INPUT_POST, 'address', FILTER_SANITIZE_STRING);
-    $website = filter_input(INPUT_POST, 'website', FILTER_SANITIZE_STRING);
-    if(isset($website)) {
-        if(!preg_match("^(htt(p|ps):\/\/)w{3}\.\w*(\.\w*)$", $website)) {
-            $_SESSION['errorMessage'] = "<p id='error-message'>Please enter a valid URL.</p>";
-            header('Location: ../view-location.php');
-        }
-    }
+    $website = filter_input(INPUT_POST, 'website', FILTER_VALIDATE_URL);
+    var_dump($website);
+    // if(!$website) {
+    //     $_SESSION['errorMessage'] = "<p id='error-message'>Please enter a valid URL.</p>";
+    //     header('Location: ../view-location.php');
+    // }
     $price = filter_input(INPUT_POST, 'price', FILTER_SANITIZE_NUMBER_INT);
+    var_dump($price);
+    $price = filter_input(INPUT_POST, 'price', FILTER_SANITIZE_NUMBER_FLOAT);
+    var_dump($price);
     $giftId = $_POST['giftid'];
     $_SESSION['giftId'] = $giftId;
 
-    $rowsChanged = addLocation($name, $address, $website, $price, $giftId);
-    if($rowsChanged != 0) {
-        $_SESSION['successMessage'] = "<p id='success-message'>New location successfully added.</p>";
-        $locationsInfo = getLocationsByGiftId($giftId);
-        $locationsList = buildLocationsList($locationsInfo);
-        $_SESSION['locationsList'] = $locationsList;
-    } else {
-        $_SESSION['errorMessage'] = "<p id='error-message'>Failed to add new location. Please try again.</p>";
-    }
-    header('Location: ../view-location.php');
+    // $rowsChanged = addLocation($name, $address, $website, $price, $giftId);
+    // if($rowsChanged != 0) {
+    //     $_SESSION['successMessage'] = "<p id='success-message'>New location successfully added.</p>";
+    //     $locationsInfo = getLocationsByGiftId($giftId);
+    //     $locationsList = buildLocationsList($locationsInfo);
+    //     $_SESSION['locationsList'] = $locationsList;
+    // } else {
+    //     $_SESSION['errorMessage'] = "<p id='error-message'>Failed to add new location. Please try again.</p>";
+    // }
+    // header('Location: ../view-location.php');
 ?>
