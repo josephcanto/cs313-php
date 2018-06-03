80457b699554b7bd47a0c5331c3c01f300d3b394
Joseph Canto <can16004@byui.edu>
2018-06-03 00:09:30 -0600

    removed testing code

--- web/php_project/library/add-location.php
+++ web/php_project/library/add-location.php
@@ -15,21 +15,16 @@
     }
     $price = filter_input(INPUT_POST, 'price', FILTER_SANITIZE_NUMBER_FLOAT);
     $giftId = $_POST['giftid'];
-    $_SESSION['giftId'] = $giftid;
-    var_dump($name);
-    var_dump($address);
-    var_dump($website);
-    var_dump($price);
-    var_dump($giftId);
+    $_SESSION['giftId'] = $giftId;
 
-    // $rowsChanged = addLocation($name, $address, $website, $price, $giftId);
-    // if($rowsChanged != 0) {
-    //     $_SESSION['successMessage'] = "<p id='success-message'>New location successfully added.</p>";
-    //     $locationsInfo = getLocationsByGiftId($giftId);
-    //     $locationsList = buildLocationsList($locationsInfo);
-    //     $_SESSION['locationsList'] = $locationsList;
-    // } else {
-    //     $_SESSION['errorMessage'] = "<p id='error-message'>Failed to add new location. Please try again.</p>";
-    // }
-    // header('Location: ../view-location.php');
+    $rowsChanged = addLocation($name, $address, $website, $price, $giftId);
+    if($rowsChanged != 0) {
+        $_SESSION['successMessage'] = "<p id='success-message'>New location successfully added.</p>";
+        $locationsInfo = getLocationsByGiftId($giftId);
+        $locationsList = buildLocationsList($locationsInfo);
+        $_SESSION['locationsList'] = $locationsList;
+    } else {
+        $_SESSION['errorMessage'] = "<p id='error-message'>Failed to add new location. Please try again.</p>";
+    }
+    header('Location: ../view-location.php');
 ?>
