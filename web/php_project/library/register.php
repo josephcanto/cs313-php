1980e57cb69b706f28a8aa7cad75a846388d69ef
Joseph Canto <can16004@byui.edu>
2018-06-05 17:47:51 -0600

    debugging password confirmation field

--- web/php_project/library/register.php
+++ web/php_project/library/register.php
@@ -11,32 +11,37 @@
         $valUserEmail = filter_var($sanUserEmail, FILTER_VALIDATE_EMAIL);
         $user_password = filter_input(INPUT_POST, 'newpassword', FILTER_SANITIZE_STRING);
         $confirm_password = filter_input(INPUT_POST, 'confirmpassword', FILTER_SANITIZE_STRING);
-        if($user_password != $confirm_password) {
-            $_SESSION['errorMessage'] = "<p id='error-message'>Passwords do not match. Please try again.</p>";
-            header('Location: ../index.php#registration');
-        }
-        $passwordHash = password_hash($user_password, PASSWORD_DEFAULT);
-        $_SESSION['firstname'] = $firstname;
 
-        $emailCheck = checkExistingEmail($valUserEmail);
-        if($emailCheck == 0) {
-            $_SESSION['errorMessage'] = "<p id='error-message'>Email address already exists in our system. Try a different one, or, log in above.</p>";
-        }
+        var_dump($user_password);
+        var_dump($confirm_password);
+        $comparisonResult = $user_password == $confirm_password;
+        var_dump($comparisonResult);
+        // if($user_password != $confirm_password) {
+        //     $_SESSION['errorMessage'] = "<p id='error-message'>Passwords do not match. Please try again.</p>";
+        //     header('Location: ../index.php#registration');
+        // }
+    //     $passwordHash = password_hash($user_password, PASSWORD_DEFAULT);
+    //     $_SESSION['firstname'] = $firstname;
 
-        $result = registerUser($valUserEmail, $passwordHash, $firstname, $lastname);
-        if($result == 1) {
-            $_SESSION['loggedIn'] = TRUE;
-            $result = getUserIdByEmail($valUserEmail);
-            $_SESSION['user_id'] = $result['id'];
-            $_SESSION['successMessage'] = "<p id='success-message'>Account created successfully. Welcome!</p>";
-            header('Location: ../dashboard.php');
-        }
-        else {
-            $_SESSION['errorMessage'] = "<p id='error-message'>Error. Registration failed. Please try again.</p>";
-            header('Location: ../index.php#registration');
-        }
-    } else {
-        $_SESSION['errorMessage'] ="<p id='error-message'>Error. You are already logged in with another account.</p>";
-        header('Location: ../index.php#registration');
-    }
+    //     $emailCheck = checkExistingEmail($valUserEmail);
+    //     if($emailCheck == 0) {
+    //         $_SESSION['errorMessage'] = "<p id='error-message'>Email address already exists in our system. Try a different one, or, log in above.</p>";
+    //     }
+
+    //     $result = registerUser($valUserEmail, $passwordHash, $firstname, $lastname);
+    //     if($result == 1) {
+    //         $_SESSION['loggedIn'] = TRUE;
+    //         $result = getUserIdByEmail($valUserEmail);
+    //         $_SESSION['user_id'] = $result['id'];
+    //         $_SESSION['successMessage'] = "<p id='success-message'>Account created successfully. Welcome!</p>";
+    //         header('Location: ../dashboard.php');
+    //     }
+    //     else {
+    //         $_SESSION['errorMessage'] = "<p id='error-message'>Error. Registration failed. Please try again.</p>";
+    //         header('Location: ../index.php#registration');
+    //     }
+    // } else {
+    //     $_SESSION['errorMessage'] ="<p id='error-message'>Error. You are already logged in with another account.</p>";
+    //     header('Location: ../index.php#registration');
+    // }
 ?>
