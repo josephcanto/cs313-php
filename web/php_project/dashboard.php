7d3190e8e562df3db25a59c109f27c8f063b77a5
Joseph Canto <can16004@byui.edu>
2018-06-09 17:02:58 -0600

    fixing some css rules

--- web/php_project/dashboard.php
+++ web/php_project/dashboard.php
@@ -153,15 +153,15 @@
             if(element.style.height == '0px') {
                 element.style.overflowY = 'visible';
                 element.style.height = '232px';
-                element.style.width = '261px';
                 element.style.padding = '2px 5px';
                 element.style.borderWidth = '0px 1px 1px 1px';
+                element.style.borderStyle = 'solid';
             } else {
                 element.style.overflowY = 'hidden';
                 element.style.height = '0px';
-                element.style.width = '0px';
                 element.style.padding = '0px';
                 element.style.borderWidth = '0px';
+                element.style.borderStyle = 'hidden';
             }
         }
     </script>
