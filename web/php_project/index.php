5e7dcaff9348af0a60fb8a533c66553f5c3071be
Joseph Canto <can16004@byui.edu>
2018-06-12 02:18:49 -0600

    changed wording in first intro paragraph

--- web/php_project/index.php
+++ web/php_project/index.php
@@ -24,7 +24,7 @@
         </section><!-- end of section 'banner' -->
         <section id='comic'>
             <h1>For-gift &amp; Forget</h1>
-            <p class='intro-paragraph'>With For-gift &amp; Forget, you can keep track of your awesome gift ideas the moment that you think of them, and then when that special occasion arrives, you'll be prepared to give the perfect gifts to your friends and family. Never forget a special occasion or a great gift idea ever again!</p>
+            <p class='intro-paragraph'>With For-gift &amp; Forget, you can keep track of your awesome gift ideas the moment you think of them, and then when that special occasion arrives, you'll be prepared to give the perfect gifts to your friends and family. Never forget a special occasion or an awesome gift idea ever again!</p>
             <div id='comic-images'>
                 <img class='comic-strip' src='images/lightbulb.png' alt='A digital drawing of a lightbulb'>
                 <img class='comic-strip' src='images/comic_2.png' alt='A digital drawing of a smartphone with the For-gift &amp; Forget app open on the screen'>
