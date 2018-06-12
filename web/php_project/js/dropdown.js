6048cb86081697ee6cbb89dc64ead59e0efc5d7c
Joseph Canto <can16004@byui.edu>
2018-06-11 23:20:17 -0600

    fixing css transition

--- web/php_project/js/dropdown.js
+++ web/php_project/js/dropdown.js
@@ -1,14 +1,14 @@
 function toggleDropdown() {
     var element = document.getElementsByClassName('form-dropdown')[0];
-    if(element.style.height == '0px') {
+    if(element.style.maxHeight == '0px') {
         element.style.overflowY = 'visible';
-        element.style.height = 'auto';
+        element.style.maxHeight = '500px';
         element.style.padding = '5px';
         element.style.borderWidth = '0px 1px 1px 1px';
         element.style.borderStyle = 'solid';
     } else {
         element.style.overflowY = 'hidden';
-        element.style.height = '0px';
+        element.style.maxHeight = '0px';
         element.style.padding = '0px';
         element.style.borderWidth = '0px';
         element.style.borderStyle = 'hidden';
